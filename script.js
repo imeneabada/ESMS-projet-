1bdda81e76dbae21035a1c41b97c96d341498b2b
mohamedhdo <mohamed.haddou.4002@gmail.com>
2025-12-03 11:12:11 +0000

    adding agenda and about

--- script.js
+++ script.js
@@ -1,18 +1,42 @@
+/* ================= About script =================*/
+const video = document.getElementById('myVideo');
+const playButton = document.getElementById('playButton');
+
+
+playButton.addEventListener('click', () => {
+    video.play();
+    video.setAttribute('controls', ''); 
+    playButton.style.display = 'none';  
+});
+
+
+video.addEventListener('pause', () => {
+    playButton.style.display = 'block';
+});
+
+
+video.addEventListener('ended', () => {
+    playButton.style.display = 'block';
+});
+
 /* ================= Agenda script =================*/
+let currentDay = 0;
+const days = document.querySelectorAll(".day");
 
+document.getElementById("rightBtn").addEventListener("click", () => {
+    days[currentDay].classList.remove("active");
+    currentDay = (currentDay + 1) % days.length;
+    days[currentDay].classList.add("active");
+});
 
-
-
-
-
-
-
-
-
+document.getElementById("leftBtn").addEventListener("click", () => {
+    days[currentDay].classList.remove("active");
+    currentDay = (currentDay - 1 + days.length) % days.length;
+    days[currentDay].classList.add("active");
+});
 
 /* ================= FAQ script ================= */
 const faqItems = document.querySelectorAll('.qstn');
-
 
 faqItems.forEach(item => {
     const question = item.querySelector('.question');
@@ -20,14 +44,7 @@ faqItems.forEach(item => {
     const icon     = item.querySelector('.reveal img');
 
     question.addEventListener('click', () => {
-
-        faqItems.forEach(other => {
-            if (other !== item) {
-                other.querySelector('.answer').classList.remove('active');
-                other.querySelector('.reveal img').classList.remove('rotate');
-            }
-        });
-
+        
         answer.classList.toggle('active');
         icon.classList.toggle('rotate');
     });
